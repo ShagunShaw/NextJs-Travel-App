82f427fe0dace17cc6d7bf6f7ff78cb745443c6a
Shagun Shaw <shagunshaw2006@gmail.com>
2026-07-04 02:21:55 +0530

    Designed more features

---
+++ src/app/page2/page.tsx
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <div>Thanks for visiting our website</div>
+  )
+}
+
+export default page
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,11 +3,11 @@ import Button from "./Button"
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-colgap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row border-2 border-red-500">
+    <section className="max-container padding-container flex flex-colgap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
       <div className="hero-map" />
 
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
-        <Image 
+        <Image
           src="/camp.svg"
           alt="camp"
           width={50}
@@ -21,7 +21,7 @@ const Hero = () => {
         <div className="my-11 flex flex-wrap gap-5">
           <div className="flex items-center gap-2">
             {Array(5).fill(1).map((_, index) => (
-              <Image 
+              <Image
                 src="star.svg"
                 key={index}
                 alt="star"
@@ -38,17 +38,45 @@ const Hero = () => {
         </div>
 
         <div className="flex flex-col w-full gap-3 sm:flex-row">
-          <Button 
+          <Button
             type="button"
             title="Download App"
             variant="btn_green"
           />
-          <Button 
+          <Button
             type="button"
             title="How we work?"
             icon="/play.svg"
             variant="btn_green"
           />
+        </div>
+      </div>
+
+      <div className="relative flex flex-1 items-start">
+        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+          <div className="flex flex-col">
+            <div className="flexBetween">
+              <p className="regular-16 text-gray-20">
+                Location
+              </p>
+
+              <Image
+                src="close.svg"
+                alt="close"
+                width={24}
+                height={24}
+              />
+            </div>
+
+            <p className="bold-20 text-white">Aguas Calientes</p>
+          </div>
+
+          <div className="flexBetween">
+            <div className="flex flex-col">
+              <p className="regular-16 block text-gray-20">Distance</p>
+              <p className="bold-20 text-white">173.4 miles</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
